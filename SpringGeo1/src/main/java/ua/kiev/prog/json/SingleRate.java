7ffe01cf9de35a502050919f9f91f30de160a251
asfdhk <134499499+asfdhk@users.noreply.github.com>
2023-09-01 22:18:07 +0300

    task
    task

---
+++ SpringGeo1/src/main/java/ua/kiev/prog/json/SingleRate.java
@@ -0,0 +1,15 @@
+package ua.kiev.prog.json;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class SingleRate {
+    @JsonProperty("UAH")
+    private double uah;
+
+    @JsonProperty("USD")
+    private double usd1;
+
+    private double usd;
+}
